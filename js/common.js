7ba1ed02f6db4c33a4eec37b221795e316a616bc
eliagbayani <eli@eol.org>
2016-02-24 06:20:12 -0500

    streamline javascript 01

--- js/common.js
+++ js/common.js
@@ -2,48 +2,84 @@ function goFullScreen()
 {
     var elem = document.getElementById("gmap"); //gmap or map-container
 
-    // if (elem.requestFullscreen)         {elem.requestFullscreen();} 
-    // else if (elem.msRequestFullscreen)  {elem.msRequestFullscreen();} 
-    // else if (elem.mozRequestFullScreen) {elem.mozRequestFullScreen();} //Firefox 
-    // else if (elem.webkitRequestFullscreen) //Chrome and Safari
-    // {
-    //     // elem.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-    //     elem.style.width = "100%";
-    //     elem.style.height = "100%";
-    //     elem.webkitRequestFullscreen();
-    // }
-
     if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) 
     {  // current working methods
-      $('goFullText').innerHTML = "Fullscreen ON";
-      if      (elem.requestFullscreen)      {elem.requestFullscreen();} 
-      else if (elem.msRequestFullscreen)    {elem.msRequestFullscreen();} 
-      else if (elem.mozRequestFullScreen)   {elem.mozRequestFullScreen();} 
-      else if (elem.webkitRequestFullscreen) {
-        elem.style.width = "100%";
-        elem.style.height = "100%";
+        $('goFullText').innerHTML = "Fullscreen ON";
+
+
+        if ($('goPanelText').innerHTML == "Panel ON")
+        {
+            $('panel').style.height      = "100%";
+            $('panel').style.width       = "17%";
+            $('map-canvas').style.height = "100%";
+            $('map-canvas').style.width  = "83%";
+
+        }
+        else
+        {
+            $('panel').style.height      = "0px";
+            $('panel').style.width       = "0px";
+            $('map-canvas').style.height = "100%";
+            $('map-canvas').style.width  = "100%";
+            
+        }
         
 
-        elem.webkitRequestFullscreen(); //Element.ALLOW_KEYBOARD_INPUT
-        // panelShowHide();
-        // panelShowHide();
-        // EoLMap.change();
-        
-      }
-    } else
+        if      (elem.requestFullscreen)      {elem.requestFullscreen();} 
+        else if (elem.msRequestFullscreen)    {elem.msRequestFullscreen();} 
+        else if (elem.mozRequestFullScreen)   {elem.mozRequestFullScreen();} 
+        else if (elem.webkitRequestFullscreen) 
+        {
+            elem.style.width = "100%";
+            elem.style.height = "100%";
+
+            // $('panel').style.height      = "100%";
+            // $('map-canvas').style.height = "100%";
+            // $('panel').style.width      = "17%";
+            // $('map-canvas').style.width = "83%";
+
+            elem.webkitRequestFullscreen(); //Element.ALLOW_KEYBOARD_INPUT
+            google.maps.event.trigger(EoLMap.map, 'resize');
+            
+            // panelShowHide();
+            // panelShowHide();
+            // EoLMap.change();
+        }
+    } 
+    else
     {
-      $('goFullText').innerHTML = "Fullscreen OFF";
-      if      (document.exitFullscreen) {document.exitFullscreen();} 
-      else if (document.msExitFullscreen) {document.msExitFullscreen();} 
-      else if (document.mozCancelFullScreen) {document.mozCancelFullScreen();} 
-      else if (document.webkitExitFullscreen) {
-        elem.style.width = "";
-        document.webkitExitFullscreen();
-      }
+          $('goFullText').innerHTML = "Fullscreen OFF";
+      
+          // alert("turning off fullscreen");
+          
+          if ($('goPanelText').innerHTML == "Panel ON")
+          {
+              $('panel').style.height      = "500px";
+              $('panel').style.width       = "400px";
+              $('map-canvas').style.height = "500px";
+              $('map-canvas').style.width  = "800px";
+              
+          }
+          else
+          {
+              $('panel').style.height      = "0px";
+              $('panel').style.width       = "0px";
+              $('map-canvas').style.height = "500px";
+              $('map-canvas').style.width  = "1200px";
+              
+          }
+          
+      
+          if      (document.exitFullscreen) {document.exitFullscreen();} 
+          else if (document.msExitFullscreen) {document.msExitFullscreen();} 
+          else if (document.mozCancelFullScreen) {document.mozCancelFullScreen();} 
+          else if (document.webkitExitFullscreen) {
+            elem.style.width = "";
+            document.webkitExitFullscreen();
+          }
     }
-
-    google.maps.event.trigger(EoLMap.map, 'resize');
     
+    google.maps.event.trigger(EoLMap.map, 'resize');
 }
 
 // start: listeners for fullscreenchanges
@@ -54,7 +90,8 @@ if (document.addEventListener) {
     document.addEventListener('MSFullscreenChange', exitHandler, false);
 }
 function exitHandler() {
-    if (document.webkitIsFullScreen || document.mozFullScreen || document.msFullscreenElement !== null)
+    // if (document.webkitIsFullScreen || document.mozFullScreen || document.msFullscreenElement !== null)
+    if(is_full_screen)
     {
         if(!document.webkitIsFullScreen)
         {
@@ -64,24 +101,92 @@ function exitHandler() {
         }
         if(document.mozFullScreen) $('goFullText').innerHTML = "Fullscreen ON2";
     }
+
+
+    if(!is_full_screen())
+    {
+        // alert("went here for all browsers");
+        if ($('goPanelText').innerHTML == "Panel ON")
+        {
+            $('panel').style.height      = "500px";
+            $('panel').style.width       = "400px";
+            $('map-canvas').style.height = "500px";
+            $('map-canvas').style.width  = "800px";
+        }
+        else
+        {
+            $('map-canvas').style.height = "500px";
+            $('map-canvas').style.width  = "1200px";
+        }
+    }
+    
+    google.maps.event.trigger(EoLMap.map, 'resize');
+    
 }
 // end: listeners for fullscreenchanges
 
+function is_full_screen()
+{
+    var elem = document.getElementById("gmap"); //gmap or map-container
+    if      (elem.requestFullscreen) {}
+    else if (elem.msRequestFullscreen)
+    {
+        if (document.msFullscreenElement == true) return true;
+    } 
+    else if (elem.mozRequestFullScreen)
+    {
+        if (document.mozFullScreen == true) return true;
+    }   
+    else if (elem.webkitRequestFullscreen)
+    {
+        if (document.webkitIsFullScreen == true) return true;
+    }
+    return false;
+}
+
 function panelShowHide()
 {
-    var el = document.getElementById("panel");
-    if ($('goPanelText').innerHTML == "Panel ON")
+    // el.style.display = 'none';
+    // el.style.display = 'block';
+    
+    if ($('goPanelText').innerHTML == "Panel ON") $('goPanelText').innerHTML = "Panel OFF";
+    else                                          $('goPanelText').innerHTML = "Panel ON";
+    
+    if (is_full_screen())
     {
-        $('goPanelText').innerHTML = "Panel OFF";
-        // el.style.display = 'none';
-        el.style.width = 0;
+        $('map-canvas').style.height = "100%";
+        if ($('goPanelText').innerHTML == "Panel ON")
+        {
+            $('panel').style.width       = "17%";
+            $('panel').style.height      = "500px";
+            $('map-canvas').style.width  = "83%";
+        }
+        else
+        {
+            $('panel').style.width = "0px";
+            $('panel').style.height = "0px";
+            $('map-canvas').style.width = "100%";
+        }
     }
     else
     {
-        $('goPanelText').innerHTML = "Panel ON";
-        // el.style.display = 'block';
-        el.style.width = "17%";
+        $('map-canvas').style.height = "500px";
+        
+        if ($('goPanelText').innerHTML == "Panel ON")
+        {
+            $('panel').style.width       = "400px";
+            $('panel').style.height      = "500px";
+            $('map-canvas').style.width  = "800px";
+        }
+        else
+        {
+            $('panel').style.width      = "0px";
+            $('panel').style.height     = "0px";
+            $('map-canvas').style.width = "1200px";
+        }
     }
+    
+    
     google.maps.event.trigger(EoLMap.map, 'resize');
 }
 
